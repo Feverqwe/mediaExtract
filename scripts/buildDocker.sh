ff5272bae1031a1b8cdc354ec72986109e7ba95c
Anton <Feverqwe@users.noreply.github.com>
2024-08-08 09:50:21 +0300

    upd docker image

--- scripts/buildDocker.sh
+++ scripts/buildDocker.sh
@@ -4,5 +4,5 @@ set -e
 
 source "$(dirname $0)/_variables.sh"
 
-docker run --rm -v ${PWD}:/opt -it golang:1.22-alpine3.19 sh -c \
+docker run --rm -v ${PWD}:/opt -it golang:1.22-alpine3.20 sh -c \
   "cd /opt && go build -o ${BINARY}"
